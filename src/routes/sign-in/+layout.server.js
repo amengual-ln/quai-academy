1b941093e024826b3a72288dd6ac7935877bd052
Lautaro Amengual <amengual.ln@gmail.com>
2025-06-14 00:41:35 -0300

    fix load import

--- src/routes/sign-in/+layout.server.js
+++ src/routes/sign-in/+layout.server.js
@@ -1,4 +1,4 @@
 // src/routes/sign-in/+layout.server.js
-import { load as parentLoad } from './+layout.server.js';
+import { load as parentLoad } from '../+layout.server.js';
 
 export const load = parentLoad;
