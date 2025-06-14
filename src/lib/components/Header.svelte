d6f5190ccd02e4c9cefe0de7980df48e0bc9dcb1
Lautaro Amengual <amengual.ln@gmail.com>
2025-06-14 13:12:47 -0300

    QuAI brand color

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between h-16 items-center">
       <div class="flex items-center">
-        <a href="/app" class="text-[#00f7ff] font-bold text-2xl">QuAI <span class="text-white font-medium">Academy</span></a>
+        <a href="/app" class="text-quai font-bold text-2xl">QuAI <span class="text-white font-medium">Academy</span></a>
       </div>
       
       <div class="flex items-center space-x-4">
